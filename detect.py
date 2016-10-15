b26c21a048d2ff0afbefcf4fccb30e86a139fef0
yasunorikudo <kchnk0@gmail.com>
2016-10-15 17:32:34 +0000

    Support multiprocess.

--- detect.py
+++ detect.py
@@ -3,47 +3,69 @@
 
 import cv2
 import json
+from multiprocessing import Process, Queue
+import numpy as np
 import requests
 from skimage import io
 from StringIO import StringIO
 
 
+def encord(frame, q):
+    img = cv2.resize(frame, (frame.shape[1] // 2, frame.shape[0] // 2))
+    img = img[:, ::-1].copy()
+
+    s = StringIO()
+    io.imsave(s, img, plugin='pil')
+    s.seek(0)
+    files = {'file': s,}
+
+    q.put([img, files])
+
+
 if __name__ == '__main__':
     cap = cv2.VideoCapture(0)
-    win = cv2.namedWindow('Object Detection', cv2.WINDOW_AUTOSIZE)
+    print('Press Esc to leave.')
+
+    # Load image
+    _, frame = cap.read()
+    q = Queue()
+    p = Process(target=encord, args=(frame, q))
+    p.start()
 
     while True:
-        ret, frame = cap.read()
-        img = cv2.resize(frame,
-                        (frame.shape[1] // 2, frame.shape[0] // 2))
+        # Get raw and encorded image.
+        img, files = q.get()
+        img = cv2.resize(img, (img.shape[1] * 2, img.shape[0] * 2))
 
-        s = StringIO()
-        io.imsave(s, img, plugin='pil')
-        s.seek(0)
+        # Encord cap image with another process.
+        _, frame = cap.read()
+        q = Queue()
+        p = Process(target=encord, args=(frame, q))
+        p.start()
 
-        files = {'file': s,}
+        # Send request to server.
         r = requests.post('http://192.168.12.34:8080/object', files=files)
-
         if r.text == '':
             continue
-        results = json.loads(r.text)
+        objs = json.loads(r.text)
 
-        for ret in results:
-            label = ret['label']
-            x1, y1, x2, y2 = ret['bbox']
+        for obj in objs:
+            label = obj['label']
+            x1, y1, x2, y2 = np.asarray(obj['bbox']) * 2
 
-            # draw label
+            # Draw label.
             t_size, b = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
             cv2.rectangle(img, (x1, y2 - t_size[1] - b),
-                         (x1 + t_size[0], y2), (255, 0, 0), -1)
-            cv2.putText(img, label, (x1, y2 - b / 2), cv2.FONT_HERSHEY_SIMPLEX,
-                       1.0, (255, 255, 255), 1)
+                          (x1 + t_size[0], y2), (255, 0, 0), -1)
+            cv2.putText(img, label, (x1, y2 - b / 2),
+                        cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 1)
 
-        img = cv2.resize(img, (img.shape[1] * 2, img.shape[0] * 2))
-        cv2.imshow(win, img)
+        cv2.imshow('Detection', img)
 
         key = cv2.waitKey(1)
         if key == 27:
+            p.terminate()
+            cap.release()
+            cv2.destroyAllWindows()
             break
-
